fb8ca1bb1252e80461da982549e27967dc813918
ahsan722505 <ahsantahir722505@gmail.com>
2023-12-16 19:53:22 +0500

    adds notification

--- app/src/main/java/com/example/recyclerview/WeatherViewModel.java
+++ app/src/main/java/com/example/recyclerview/WeatherViewModel.java
@@ -1,6 +1,15 @@
 package com.example.recyclerview;
 
 // WeatherViewModel.java
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
@@ -10,9 +19,17 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class WeatherViewModel extends ViewModel {
+public class WeatherViewModel extends AndroidViewModel {
 
     private final MutableLiveData<Double> temperatureLiveData = new MutableLiveData<>();
+
+    public WeatherViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+
+
+
 
     public MutableLiveData<Double> getTemperatureLiveData() {
         return temperatureLiveData;
@@ -39,6 +56,7 @@ public class WeatherViewModel extends ViewModel {
                         double temperature = weatherDetails.getTemperature();
                         double temperatureInCelsius = Math.round(temperature - 273.15);
                         temperatureLiveData.setValue(temperatureInCelsius);
+                        showTemperatureNotification(temperatureInCelsius, city);
                     }
                 }else {
                     temperatureLiveData.setValue(null);
@@ -51,6 +69,30 @@ public class WeatherViewModel extends ViewModel {
                 temperatureLiveData.setValue(null);
             }
         });
+    }
+    private void showTemperatureNotification(double temperature, String city) {
+        Context context = getApplication().getApplicationContext();
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Create a notification channel for Android Oreo and above
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String channelId = "weather_channel";
+            CharSequence channelName = "Weather Channel";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(channelId, channelName, importance);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        // Build the notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "weather_channel")
+                .setSmallIcon(R.drawable.splash_image)
+                .setContentTitle("Weather Update")
+                .setContentText(city + "'s " + "Temperature: " + temperature + " °C")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        // Show the notification
+        notificationManager.notify(1, builder.build());
     }
 
 }
